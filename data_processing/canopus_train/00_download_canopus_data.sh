736e9a90d38da22808e15ddbe2c3ef0187192821
sam goldman <samgoldman97@gmail.com>
2023-09-07 09:41:09 -0400

    Update downloads with permanent zenodo links

--- data_processing/canopus_train/00_download_canopus_data.sh
+++ data_processing/canopus_train/00_download_canopus_data.sh
@@ -2,6 +2,7 @@
 #SVM_URL="https://bio.informatik.uni-jena.de/wp/wp-content/uploads/2020/08/svm_training_data.zip"
 
 export_link="https://www.dropbox.com/scl/fi/ddwa659ywqrsiolv9pxje/canopus_train_export_v2.tar?rlkey=1qgithmd6sztivwuqr4c505h5"
+export_link="https://zenodo.org/record/8316682/files/canopus_train_export_v2.tar"
 
 cd data/paired_spectra/
 wget -O canopus_train_export.tar $export_link
